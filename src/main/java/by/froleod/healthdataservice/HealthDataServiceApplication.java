5cca2cc71934ccd3a2d14a4c32451b86ea0b2e09
LFrolov <froleod@gmail.com>
2025-11-04 22:11:16 +0300

    init

---
+++ src/main/java/by/froleod/healthdataservice/HealthDataServiceApplication.java
@@ -0,0 +1,13 @@
+package by.froleod.healthdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthDataServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/froleod/healthdataservice/HealthDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.froleod.healthdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthDataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
